4fbc2405f6c0e7d32392fa1b6b658121199072d7
Jose Hernandez <Jose Hernandez@DESKTOP-SIE4EFC>
2026-05-28 10:08:16 -0600

    Loading screen. new update en layout.

--- ISLAGO_V3/wwwroot/js/site.js
+++ ISLAGO_V3/wwwroot/js/site.js
@@ -22,3 +22,79 @@ if (toggleBtn) {
         sidebar.classList.toggle("show");
     });
 }
+
+/* ========================================= */
+/* =============== LOADER ================== */
+/* ========================================= */
+
+window.addEventListener("load", () => {
+
+    const loader = document.getElementById("page-loader");
+
+    const percentText = document.getElementById("loader-percent");
+
+    const progressCircle = document.querySelector(".progress-ring-fill");
+
+    const loaderText = document.getElementById("loader-text");
+
+    if (!loader) return;
+
+    const radius = 60;
+
+    const circumference = 2 * Math.PI * radius;
+
+    let progress = 0;
+
+    const messages = [
+        "Inicializando módulo...",
+        "Cargando recursos...",
+        "Procesando información...",
+        "Preparando interfaz...",
+        "Finalizando carga..."
+    ];
+
+    const interval = setInterval(() => {
+
+        progress += Math.floor(Math.random() * 8) + 3;
+
+        if (progress > 100)
+            progress = 100;
+
+        percentText.textContent = `${progress}%`;
+
+        const offset =
+            circumference - (progress / 100) * circumference;
+
+        progressCircle.style.strokeDashoffset = offset;
+
+        // Cambiar mensajes dinámicamente
+        if (progress < 25)
+            loaderText.textContent = messages[0];
+
+        else if (progress < 45)
+            loaderText.textContent = messages[1];
+
+        else if (progress < 70)
+            loaderText.textContent = messages[2];
+
+        else if (progress < 90)
+            loaderText.textContent = messages[3];
+
+        else
+            loaderText.textContent = messages[4];
+
+        // FINALIZAR
+        if (progress >= 100) {
+
+            clearInterval(interval);
+
+            setTimeout(() => {
+
+                loader.classList.add("hide");
+
+            }, 400);
+        }
+
+    }, 80);
+
+});
